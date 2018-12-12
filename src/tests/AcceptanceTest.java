d021f4ebed4dcd4223be35b919b5390285608515
Chris Wall <Chris Wall@LAPTOP-JG0P250U>
2018-12-12 18:29:22 -0500

    Acceptance test written

--- src/tests/AcceptanceTest.java
+++ src/tests/AcceptanceTest.java
@@ -2,11 +2,19 @@ package tests;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.net.InetAddress;
+import java.net.Socket;
+import java.net.UnknownHostException;
+
 import org.junit.jupiter.api.Test;
 
 import boardClient.BoardClient;
 import boardClient.FXMLDocumentController;
 import boardServer.BoardServer;
+import board.BoardConstants;
+
 
 class AcceptanceTest {
 
@@ -18,8 +26,33 @@ class AcceptanceTest {
 		boardServer.FXMLDocumentController serverDocumentController = new boardServer.FXMLDocumentController();
 		boardClient.FXMLDocumentController clientDocumentController = new boardClient.FXMLDocumentController();
 		
-		clientDocumentController.storyID.setText("1374");
-		assert(true);
+		serverDocumentController.initialize(null, null);
+		
+		assert(serverDocumentController.clientNo==0);
+		
+		serverDocumentController.test=1;
+		InetAddress addr;
+		Socket socket;
+		PrintWriter outputToServer;
+		try {
+			addr = InetAddress.getByName("localhost");
+			try {
+				socket = new Socket(addr, 8000);
+				outputToServer = new PrintWriter(socket.getOutputStream());
+				outputToServer.println(BoardConstants.SEND_HANDLE);
+				outputToServer.println("handle");
+				outputToServer.flush();
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		} catch (UnknownHostException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		System.out.println(serverDocumentController.clientNo);
+		assert(serverDocumentController.clientNo==1);
+		
 	}
 
 }
